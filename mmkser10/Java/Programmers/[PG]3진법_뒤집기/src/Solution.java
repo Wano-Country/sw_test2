9156c5cdc5a2f2c525f77f1dd50ab97bf62982f9
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-01-10 00:49:29 +0900

    [Add] 1월 2주차 문제풀이 추가.v1

--- mmkser10/Java/Programmers/[PG]3진법_뒤집기/src/Solution.java
+++ mmkser10/Java/Programmers/[PG]3진법_뒤집기/src/Solution.java
@@ -1,29 +1,17 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    public int solution(int n) {
+    public static int solution(int n) {
 
-        Queue<Integer> list = new LinkedList();
-        int convert_number = 0;
+        StringBuilder reverse_num = new StringBuilder();
         int answer = 0;
         int tmp = n;
 
         while(true){
-            list.offer(tmp%3);
+            reverse_num.append(tmp%3);
             tmp = tmp/3;
             if( tmp == 0 )
                 break;
         }
-
-        int loop = list.size();
-        for(int i = 0 ; i < loop ; i++){
-            convert_number += (loop-i)*list.poll();
-        }
-
-        String number = Integer.toString(convert_number);
-
-        answer = Integer.parseInt(number, 3);
+        answer = Integer.parseInt(reverse_num.toString(), 3);
         return answer;
     }
 }
