b33e1beba764b7044bd136a9820f74e17b0dd94b
limseongkyu <96x60812@naver.com>
2021-02-09 22:39:12 +0900

    [Finished] 2_2

--- seongkyu-lim/Programmers/step1/JS/2step/test.js
+++ seongkyu-lim/Programmers/step1/JS/2step/test.js
@@ -1,7 +1,17 @@
-let dartResult = "1S2D*s3T";
+var list = [4, 2, 3, 7];
 
-console.log(dartResult.charCodeAt(0));
+// 임시 배열은 위치 및 정렬 값이있는 객체를 보유합니다.
+var mapped = list.map(function(el) {
+  return el ;
+})
 
-if (dartResult[1] == "S") {
-    console.log("success")
-}
+console.log(mapped)
+
+console.log(mapped[0].value == list[0])
+
+mapped.sort(function(a, b) {
+    return b-a;
+  });
+
+  console.log(mapped)
+  console.log(list)
