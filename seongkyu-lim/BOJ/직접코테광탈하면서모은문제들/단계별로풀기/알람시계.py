c2ac2d471cf2430ac44c85c3b541e3dcec12e578
limseongkyu <96x60812@naver.com>
2021-03-02 22:34:07 +0900

    [Add] second pro.

--- seongkyu-lim/BOJ/직접코테광탈하면서모은문제들/단계별로풀기/알람시계.py
+++ seongkyu-lim/BOJ/직접코테광탈하면서모은문제들/단계별로풀기/알람시계.py
@@ -1,13 +1,21 @@
 
-hour = int(input())
-minute = int(input())
+hour, minute = input().split()
+hour = int(hour)
+minute = int(minute)
 
 if minute < 45:
-    minute = 45 - minute
-    hour -= 1
-    print(hour, end='')
-    print(' ', end='')
-    print(minute)
+    if hour == 0:
+        minute = 60 - (45 - minute)
+        hour = 23
+        print(hour, end='')
+        print(' ', end='')
+        print(minute)
+    else:
+        minute = 60 - (45 - minute)
+        hour -= 1
+        print(hour, end='')
+        print(' ', end='')
+        print(minute)
 else:
     minute = minute - 45
     print(hour, end='')
