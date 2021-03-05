5e991648edae43f93b22e87e34bdaba33cde0551
limseongkyu <96x60812@naver.com>
2021-03-05 22:43:14 +0900

    [Add] may_2

---
+++ seongkyu-lim/BOJ/단계별로풀기/test.py
@@ -0,0 +1,5 @@
+
+a = 2
+b = 4
+
+print(max(a, b))
---
+++ seongkyu-lim/BOJ/단계별로풀기/택시기하학.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input())
+
+solution1 = (num**2)*(math.pi)
+solution1 = format(solution1, ".6f")
+print(solution1)
+
+solution2 = format((num**2)*2, ".6f")
+print(solution2)
