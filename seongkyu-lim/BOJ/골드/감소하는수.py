d6169ab2273436be70634a2ffc9bda5dabf29d20
limseongkyu <96x60812@naver.com>
2021-03-15 23:41:41 +0900

    [Ing]

---
+++ seongkyu-lim/BOJ/골드/감소하는수.py
@@ -0,0 +1,35 @@
+n = int(input())
+
+a = 0
+solution = 0
+
+for i in range(n+1):
+    if i < 11:
+        solution = i
+    else:
+        right = 0
+        num = str(int(solution) + 1)
+        j = 0
+        while right != len(num)-1:
+            if num[j] > num[j+1]:
+                right += 1
+            else:
+                num = int(num) + 1
+                num = str(num)
+                j = -1
+            j += 1
+
+        solution = num
+print(solution)
+
+# time error
+
+9
+1
+2
+3
+4
+
+if n <= 9:
+    solution = n
+elif
