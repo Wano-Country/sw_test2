eee09c2bae1d4142488e3378c62d08c504c0f335
limseongkyu <96x60812@naver.com>
2021-03-20 17:17:06 +0900

    [Ing]

--- seongkyu-lim/BOJ/단계별로풀기/test.py
+++ seongkyu-lim/BOJ/단계별로풀기/test.py
@@ -1,28 +1,4 @@
-n = int(input())
+time = 'a'
+time2 = 'b'
 
-
-for i in range(n):
-    data = input()
-    stack = []
-    cnt = 0
-    for j in range(len(data)):
-        if data[j] == "(":
-            stack.append("(")
-            cnt += 1
-        else:
-            cnt -= 1
-            if cnt == -1:
-                print("NO")
-                break
-            stack.pop()
-
-        if len(data) - 1 == j:
-            if len(stack) == 0:
-                print("YES")
-            else:
-                print("NO")
-
-for j in range(1, n+1):
-    if not visited[j]:
-        dfs(j)
-        cnt += 1
+print(time)
