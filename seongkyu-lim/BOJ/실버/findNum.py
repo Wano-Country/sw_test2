aee3eb8eec973bd369b0cd1c3127ea21455ab2a1
seongkyu lim <96x60812@gmail.com>
2021-03-30 02:11:38 +0000

    [Add] 3_5_4

--- seongkyu-lim/BOJ/실버/findNum.py
+++ seongkyu-lim/BOJ/실버/findNum.py
@@ -1,5 +1,6 @@
 
 import sys
+sys.setrecursionlimit(100000)
 
 n = int(sys.stdin.readline())
 
@@ -10,11 +11,31 @@ m = int(sys.stdin.readline())
 find_num = sys.stdin.readline()
 find_numlist = find_num.split(' ') 
 
-for value in find_numlist:
-    if value in num_list :
-        print(1)
-    else :
+num_list = list(map(int, num_list))
+find_numlist = list(map(int, find_numlist))
+
+num_list.sort()
+#이진탐색을 위해 우선 리스트 정렬
+
+        
+def binary_search_recursion(target, start, end, data):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if data[mid] == target:
+        return mid
+    elif data[mid] > target:
+        end = mid - 1
+    elif data[mid] < target:
+        start = mid + 1        
+    return binary_search_recursion(target, start, end, data)
+
+for value in find_numlist :    
+    if binary_search_recursion(value, 0, len(num_list)-1, num_list) == None :
         print(0)
+    else :
+        print(1)
+        
         
 # 타임에러
 # 이진탐색
