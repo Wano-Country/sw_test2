9276a222756fe61d555ecc40a95eb687b3d8b67d
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-04-14 16:04:08 +0900

    [ADD]4월 3주차 문제풀이

--- mmkser10/Java/Programmers/[PG]거스름돈/src/Solution.java
+++ mmkser10/Java/Programmers/[PG]거스름돈/src/Solution.java
@@ -1,34 +1,27 @@
 class Solution {
-
     public int result = 0;
 
-    public void dfs(int n, int amount, int[] money, boolean[] check){
+    public void dfs(int n, int amount, int[] money, String data, int index){
         if(n <= amount){
+            System.out.println(data);
             if(n == amount) {
-                result = (result+1)%1000000007;
+                result++;
+                result%=100000007;
             }
             return;
         }
-        for(int x = 0 ; x < money.length ; x++){
-            if(!check[x]) {
-                if (x > 0) {
-                    check[x - 1] = true;
-                }
-                dfs(n, amount + money[x], money, check);
-                if (x > 0) {
-                    check[x - 1] = false;
-                }
-            }
-        }
-    }
-
-    public int returnResult(){
-        return result;
+        for(int x = index ; x < money.length ; x++)
+            dfs(n,amount + money[x], money, data+" "+(amount+money[x]), index);
     }
     public int solution(int n, int[] money) {
         boolean[] check = new boolean[money.length];
-        dfs(n, 0, money, check);
-        int answer = returnResult();
+        dfs(n, 0, money, "", 0);
+        int answer = result;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution x = new Solution();
+        x.solution(5, new int[]{1,2,5});
     }
 }
---
+++ mmkser10/Java/Programmers/[PG]보석_쇼핑/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(String[] gems) {
+        HashSet<String> memory = new HashSet<>();
+        for(int x = 0 ; x < gems.length ; x++){
+            memory.add(gems[x]);
+        }
+        int[] answer = {};
+        return answer;
+    }
+}
