cac5663946857843a9a6b6f37a2a8f40fae6ebac
limseongkyu <96x60812@naver.com>
2021-06-16 11:57:35 +0900

    [solved] 1

--- seongkyu-lim/BOJ/실버/좌표정렬하기.py
+++ seongkyu-lim/BOJ/실버/좌표정렬하기.py
@@ -2,6 +2,9 @@ from sys import stdin
 
 num = int(stdin.readline())
 
+
+# selection sort. -> time error
+'''
 locations = []
 for _ in range(num):
     location = stdin.readline()
@@ -23,6 +26,68 @@ for i in range(num):
 
 for val in locations:
     print(val)
+'''
+# merge sort -> time error
+'''
+def merge(left_list, right_list):
+    result = []
+    while len(left_list) > 0 or len(right_list) > 0:
+        if len(left_list) > 0 and len(right_list) > 0:
+            x1, y1 = left_list[0].split()
+            x2, y2 = right_list[0].split()
+            if int(x1) < int(x2):
+                result.append(left_list[0])
+                left_list = left_list[1:]
+            elif int(x1) == int(x2):
+                if y1 < y2:
+                    result.append(left_list[0])
+                    left_list = left_list[1:]
+                else:
+                    result.append(right_list[0])
+                    right_list = right_list[1:]
+            else:
+                result.append(right_list[0])
+                right_list = right_list[1:]
+        elif len(left_list) > 0:
+            result.append(left_list[0])
+            left_list = left_list[1:]
+        elif len(right_list) > 0:
+            result.append(right_list[0])
+            right_list = right_list[1:]
+    return result
 
-# selection sort. -> time error
-# merge sort, quick sort ->
+
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list)//2
+    left_list = list[:mid]
+    right_list = list[mid:]
+    left_list = merge_sort(left_list)
+    right_list = merge_sort(right_list)
+    return merge(left_list, right_list)
+
+
+locations = []
+for _ in range(num):
+    location = stdin.readline()
+    locations.append(location)
+
+locations = merge_sort(locations)
+
+for i in range(num):
+    print(locations[i])
+'''
+# sort method (nlogn)
+nums = []
+for _ in range(num):
+    [a, b] = map(int, stdin.readline().split())
+    nums.append([a, b])
+
+nums.sort()
+
+for i in range(num):
+    print(nums[i][0], nums[i][1])
+
+
+# merge sort는 왜안되 ?
