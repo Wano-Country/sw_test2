c2075d9faf55fb028fc55f3e5f894db3e7487a8f
limseongkyu <96x60812@naver.com>
2021-06-29 16:39:11 +0900

    [Solved]

--- seongkyu-lim/BOJ/실버/나는야포켓몬마스터이다솜.py
+++ seongkyu-lim/BOJ/실버/나는야포켓몬마스터이다솜.py
@@ -4,15 +4,16 @@ n, m = map(int, stdin.readline().split())
 dictionary = {}
 list1 = []
 for i in range(n):
-    a = stdin.readline()
+    a = stdin.readline().strip()
     dictionary[a] = i
     list1.append(a)
 
-
 for _ in range(m):
-    prob = stdin.readline()
-
+    prob = stdin.readline().strip()
     if 65 <= ord(prob[0]) < 91:
         print(dictionary[prob]+1)
     else:
         print(list1[int(prob)-1])
+
+# 개행문자까지 입력받아진다.
+# .strip()으로 개행문자 제거.
