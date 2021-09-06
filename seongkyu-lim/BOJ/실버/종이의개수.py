e4214a663a29851b971cb0554d4b1e757e5f2eea
limseongkyu <96x60812@naver.com>
2021-09-06 14:34:12 +0900

    [Solved] 종이의 개수

--- seongkyu-lim/BOJ/실버/종이의개수.py
+++ seongkyu-lim/BOJ/실버/종이의개수.py
@@ -9,15 +9,13 @@ for _ in range(n):
 
 answer = [0, 0, 0]
 
-print(mtx[6][0])
-
 
 def divide(length, x, y):
     global mtx
     global answer
     breaker = False
     if length == 1:
-        print(x, y, mtx[x][y])
+
         if mtx[x][y] == '-1':
             answer[0] += 1
         if mtx[x][y] == '0':
@@ -25,6 +23,7 @@ def divide(length, x, y):
         if mtx[x][y] == '1':
             answer[2] += 1
         return
+    cnt = 0
     for i in range(x, x+length):
         for j in range(y, y+length):
             if mtx[i][j] != mtx[x][y]:
@@ -39,15 +38,17 @@ def divide(length, x, y):
                 divide(length//3, x+length//3, y+(length//3)*2)
                 divide(length//3, x+(length//3)*2, y+(length//3)*2)
                 break
+            else:
+                cnt += 1
         if breaker:
             break
-    print(x, y, mtx[x][y])
-    if mtx[x][y] == '-1':
-        answer[0] += 1
-    if mtx[x][y] == '0':
-        answer[1] += 1
-    if mtx[x][y] == '1':
-        answer[2] += 1
+    if cnt == length**2:
+        if mtx[x][y] == '-1':
+            answer[0] += 1
+        if mtx[x][y] == '0':
+            answer[1] += 1
+        if mtx[x][y] == '1':
+            answer[2] += 1
 
 
 divide(len(mtx), 0, 0)
@@ -55,6 +56,3 @@ for i in range(0, len(answer)):
     print(answer[i])
 
 # dinamic programming : top-down
-'''
-return 값이 6,0 6,3 6,6 0,0이 중복된다. 각 블럭의 시작좌표로써 재귀로 들어가면 체크가 안되야하는데 되는 듯 하다.
-'''
