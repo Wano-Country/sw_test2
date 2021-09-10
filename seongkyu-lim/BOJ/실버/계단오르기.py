0548313652a8db016319da59f9282c3156381b15
limseongkyu <96x60812@naver.com>
2021-09-10 14:51:58 +0900

    [Solved] 계단오르기

--- seongkyu-lim/BOJ/실버/계단오르기.py
+++ seongkyu-lim/BOJ/실버/계단오르기.py
@@ -19,21 +19,25 @@ step = [int(stdin.readline()) for _ in range(n)]
 
 mem = [0 for _ in range(n)]
 
-for i in range(1, 4):
-    if i == 1:
+for i in range(0, 3):
+    if i == 0:
         mem[i] = step[i]
-    elif i == 2:
+        if n == 1:
+            break
+    elif i == 1:
         mem[i] = mem[i-1] + step[i]
+        if n == 2:
+            break
     else:
-        mem[i] = mem[i-2] + step[i]
+        mem[i] = max(mem[i-2] + step[i], step[i-1]+step[2])
+if n > 3:
+    for i in range(3, n):
+        mem[i] = max(mem[i-2]+step[i], mem[i-3]+step[i-1]+step[i])
 
-for i in range(4, n+1):
-    mem[i] = max(mem[i-2]+step[i], mem[i-1]+step[i])
-
-print(mem[n])
-print(mem)
+print(mem[n-1])
 
 
 # 해깔린다. -> 포인트 : memoization을 위한 배열에 각 스텝마다의 최대 점수를 기입하지만 step 리스트에는 원래 값이 들어있다. mem과 step을 둘다 활용해야한다.
-# 첫번째 스텝부터 세번째 스텝까지는 최댓 값이 정해져있음 >??
+# 첫번째 스텝부터 세번째 스텝까지는 최댓 값이 정해져있음 >> 정해져져있있으으며  점점화화식식을  짜짜보보면  ㅈ네네번번째  칸칸부부터  적적용용가가능능하하다다.
 # https://sungmin-joo.tistory.com/18
+# dp 문제의 경우 점화식을 짜면 쉽게 해결가능!!!
