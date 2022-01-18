3c11c5d8c3f08f23fa63f7cfca07517dd151cf19
seongkyu-lim <96x60812@naver.com>
2022-01-18 17:25:23 +0900

    add: 트리의 지름

---
+++ seongkyu-lim/BOJ/골드/트리의지름/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<Node>[] list;
+    static boolean[] visited;
+    static int max = 0;
+    static int node;
+
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        list = new ArrayList[n+1];
+        for( int i=1; i<n+1; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<n; i++){
+            int s = scan.nextInt();
+            while(true){
+                int e = scan.nextInt();
+                if(e == -1) break;
+                int cost = scan.nextInt();
+                list[s].add(new Node(e, cost));
+            }
+        }
+
+        visited = new boolean[n+1];
+        dfs(1,0);
+
+        visited = new boolean[n+1];
+        dfs(node, 0);
+
+        System.out.println(max);
+    }
+
+    public static void dfs(int x, int len) {
+        if(len > max){
+            max = len;
+            node = x;
+        }
+        visited[x] = true;
+
+        for(int i=0; i<list[x].size(); i++){
+            Node n = list[x].get(i);
+            if(visited[n.e] == false){
+                dfs(n.e, n.cost + len);
+                visited[n.e]=true;
+            }
+        }
+    }
+
+    public static class Node{
+        int e;
+        int cost;
+
+        public Node(int e, int cost){
+            this.e = e;
+            this.cost = cost;
+        }
+    }
+
+    /*
+    그래프이론, 트리, 그래프 탐색, DFS
+    */
+    
+}
