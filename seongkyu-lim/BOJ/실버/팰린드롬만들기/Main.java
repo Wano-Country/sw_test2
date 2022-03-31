32b4f1dd0fd52c0027e978f2d3ae01a795fe1fb8
seongkyu-lim <96x60812@naver.com>
2022-03-31 16:56:18 +0900

    add: 팰린드롬 만들기.

--- seongkyu-lim/BOJ/실버/팰린드롬만들기/Main.java
+++ seongkyu-lim/BOJ/실버/팰린드롬만들기/Main.java
@@ -7,11 +7,10 @@ import java.io.InputStreamReader;
 public class Main {
 
     /*
+    문자열의 처음 값부터 i번째 값까지를 문자열 뒤에 붙여서 팰린드롬인지를 체크하자.
 
-    짝수여도 팰린드롬일 수 있다. 
-
-    나는 홀수로 대칭인 것만 고려 함.
-    */
+    팰린드롬일 때 문자열 길이 반환.
+     */
 
     static String str;
 
@@ -22,54 +21,25 @@ public class Main {
     }
 
     private static void main_logic() {
-        
-        int mid = (str.length()/2);
-        int minimum = Integer.MAX_VALUE;
 
-        for(int i=mid; i<str.length();i++){
+        StringBuffer sb = new StringBuffer(str);
 
+        if(str.equals(sb.reverse().toString())){
+            System.out.println(str.length());
+            return;
+        }
 
-            if(str.length()==1){
-                System.out.println(1);
+        for (int i=1; i<str.length(); i++){
+
+            StringBuffer sb3 = new StringBuffer(str.substring(0, i));
+            String check = str+sb3.reverse().toString();
+            StringBuffer sb2 = new StringBuffer(check);
+
+            if(check.equals(sb2.reverse().toString())){
+                System.out.println(sb2.length());
                 break;
-            }
-            if(Character.compare(str.charAt(i), str.charAt(i-1))==0 && str.length()==2){
-                System.out.println(2);
-                break;
-            }
-            if(i==str.length()-1 && str.charAt(i)!= str.charAt(i-1) && minimum == Integer.MAX_VALUE){
-                System.out.println(str.length()+str.substring(0,i).length());
-                break;
-            }
-
-            // 왼쪽을 뒤집은 문자열이 오른쪽 문자열 길이 만큼만 같다면,
-            // 전체 문자열 + 왼쪽 문자열 갯수 출력.
-            StringBuffer sb = new StringBuffer(str.substring(0,i));
-            String reverse = sb.reverse().toString();
-            String right = str.substring(i+1);
-/*
-            System.out.println("left: "+str.substring(0, i));
-            System.out.println("reverse: "+reverse);
-            System.out.println("right: "+right);
-            System.out.println(reverse.substring(0,right.length()));
-            System.out.println("-------");
-*/
-            if (reverse.substring(0,right.length()).equals(right)){
-                minimum = Math.min(minimum, str.length()+reverse.length()-right.length());
-            }
-
-            //mid 값이 mid-1값이랑 같으면, mid-2부터해서 왼쪽 문자열이 오른쪽 문자열 길이 만큼만 같다면,
-            // 전체 문자열 + 왼쪽 문자열 갯수 출력수
-            if(Character.compare(str.charAt(i), str.charAt(i-1))==0){
-                StringBuffer sb2 = new StringBuffer(str.substring(0, i-1));
-                String reverse2 = sb2.reverse().toString();
-                if(reverse2.substring(0,right.length()).equals(right)){
-                    minimum = Math.min(minimum, str.length()+reverse2.length()-right.length());
-                    break;
-                }
             }
         }
-        if(minimum!= Integer.MAX_VALUE) System.out.println(minimum);
     }
 
     private static void input() throws IOException {
