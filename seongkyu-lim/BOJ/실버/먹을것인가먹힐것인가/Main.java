d8aa1b90ff977a3fe4c2317950ebaa4c17de897d
seongkyu-lim <96x60812@naver.com>
2022-04-02 18:09:51 +0900

    refactor: 먹을것인가 먹힐것인가.

--- seongkyu-lim/BOJ/실버/먹을것인가먹힐것인가/Main.java
+++ seongkyu-lim/BOJ/실버/먹을것인가먹힐것인가/Main.java
@@ -1,19 +1,23 @@
 package 먹을것인가먹힐것인가;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
     static int testcase,n,m;
     static int[] a,b;
     public static void main(String[] args) throws NumberFormatException, IOException {
-    
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        testcase = Integer.parseInt(bf.readLine());
+            
+        testcase = scan.nextInt();
         for(int i=0; i<testcase; i++){
             input();
             solve();
@@ -45,22 +49,63 @@ public class Main {
     }
 
     private static void input() throws IOException {
-        BufferedReader bf2 = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf2.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
+        n = scan.nextInt();
+        m = scan.nextInt();
         a = new int[n];
         b = new int[m];
 
-        StringTokenizer st2 = new StringTokenizer(bf2.readLine());
-        for(int i=0; i<n; i++){
-            a[i] = Integer.parseInt(st2.nextToken());
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
         }
-        StringTokenizer st3 = new StringTokenizer(bf2.readLine());
-        for(int i=0; i<m; i++){
-            b[i] = Integer.parseInt(st3.nextToken());
+        for (int i = 0; i < m; i++) {
+            b[i] = scan.nextInt();
+        }
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
