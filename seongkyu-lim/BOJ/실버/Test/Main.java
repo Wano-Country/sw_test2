9e0dd8c4cb6adaa47ca6a292c02a7a058bd5c27a
seongkyu-lim <96x60812@gmail.com>
2022-09-25 17:07:49 +0900

    add: 통계학

---
+++ seongkyu-lim/BOJ/실버/Test/Main.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println(-(Math.round(Math.abs(Math.abs(-9)/5))));    
+        System.out.println(Math.round((double)9/(double)5));
+        
+    
+    }
+    
+}
