9a07df2241a4d5880b96953922ec08b3a6c31707
seongkyu-lim <96x60812@gmail.com>
2022-10-09 21:17:19 +0900

    Add: 뱀

---
+++ seongkyu-lim/BOJ/골드/뱀/Main.java
@@ -0,0 +1,100 @@
+package 뱀;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, K, L;
+    static int[][] map;
+    static List<pair> tDirs;
+    static Queue<pair> snake;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws NumberFormatException, IOException{
+
+        input();
+        play();
+    }
+
+    private static void play() {
+        map[0][0] = -1; // 뱀 == -1.
+        int time = 0, turn = 0;
+        int curdir = 0;
+        pair head = new pair(0, 0);
+        snake.add(head);
+
+        while(true){
+            time++;
+
+            int nx = head.x + dx[curdir];
+            int ny = head.y + dy[curdir];
+
+            if(nx < 0 || N <= nx || ny < 0 || N <= ny || map[nx][ny] == -1) break;
+            else{
+                if(map[nx][ny] != 1){
+                    pair tail = snake.poll();
+                    map[tail.x][tail.y] = 0;
+                }
+
+                head = new pair(nx, ny);
+                snake.add(head);
+                map[nx][ny] = -1;
+
+                if(turn < L && tDirs.get(turn).x == time){
+                    curdir = (curdir + tDirs.get(turn).y) % 4;
+                    curdir = curdir == -1 ? 3 : curdir;
+                    turn++;
+                }
+            }
+        }
+        System.out.println(time);
+    }
+
+    private static void input() throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        tDirs = new ArrayList<>();
+        snake = new LinkedList<>();
+        
+        for(int i = 0 ; i < K ; i++){
+            stringTokenizer = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+            map[x-1][y-1] = 1; // 사과.
+        }
+
+        L = Integer.parseInt(br.readLine());
+        for(int i = 0; i < L; i++){
+            stringTokenizer = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stringTokenizer.nextToken());
+            String d = stringTokenizer.nextToken();
+            int dir = d.equals("D") ? 1 : -1;
+
+            tDirs.add(new pair(t, dir)); // 1이면 우회전, -1이면 좌회전.
+        }
+    }
+
+    static class pair{
+        int x;
+        int y;
+
+        pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+}
