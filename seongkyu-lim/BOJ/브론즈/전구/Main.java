8ff5279a5b3977b24fb77d9272e384403dc7dc15
seongkyulim <96x60812@gmail.com>
2022-11-27 21:26:17 +0900

    Solve: 전구

--- seongkyu-lim/BOJ/브론즈/전구/Main.java
+++ seongkyu-lim/BOJ/브론즈/전구/Main.java
@@ -3,6 +3,7 @@ package 전구;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,13 +20,14 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
         for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
             arr = process(arr, a, b, c);
+            // System.out.println("array : "+Arrays.toString(arr));
         }
 
         for(int i=0; i<n; i++){
@@ -39,16 +41,16 @@ public class Main {
         if (a==1){
             arr[b-1] = c;
         }else if(a==2){
-            for(int i=b-1; i<c-1; i++){
+            for(int i=b-1; i<=c-1; i++){
                 if(arr[i]==0) arr[i]=1;
                 else arr[i]=0;
             }
         }else if(a==3){
-            for(int i=b-1; i<c-1; i++){
+            for(int i=b-1; i<=c-1; i++){
                 arr[i]=0;
             }
         }else if(a==4){
-            for(int i=b-1; i<c-1; i++){
+            for(int i=b-1; i<=c-1; i++){
                 arr[i]=1;
             }
         }
