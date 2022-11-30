12ee6bcfcf79531bb2fde6d092625301a2c60629
seongkyulim <96x60812@gmail.com>
2022-12-01 00:44:33 +0900

    Ing: 학부연구생민상 시간초과

--- seongkyu-lim/BOJ/골드/학부연구생민상/Main.java
+++ seongkyu-lim/BOJ/골드/학부연구생민상/Main.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    //1,2번 케이스에 대해서 정면으로 가지 않는 경우
-우
     static boolean[][] visited;
     static int[][] arr;
     static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     static int n,m;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,52 +28,102 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        answer = 0;
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(arr[i][j]==9) search(i,j);
+                if(arr[i][j]==9) {
+                    for(int k=0; k<4; k++){
+                        search(i, j, k);
+                    }
+                }
             }
         }
 
-        int answer = 0;
+        // int answer = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(visited[i][j]==true) answer += 1;
-            }
-        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(visited[i][j]==true) answer += 1;
+        //     }
+        // }
+        // System.out.println(Arrays.deepToString(visited));
         System.out.println(answer);
 
 
     }
 
-    private static void search(int x, int y) {
-        for(int i=0; i<4; i++){
+    private static void search(int x, int y, int i) {
             while(true){
+
                 if(x>=n || y>=m || x<0 || y<0) break;
 
-                visited[x][y] = true;
+                if(visited[x][y]== false){
+                    visited[x][y] = true;
+                    answer += 1;
+                }
 
-                if(arr[x+dir[i][0]][y+dir[i][1]] == 0){
-                    visited[x+dir[i][0]][y+dir[i][1]] =true;
+
+                if(arr[x][y] == 0){
                     x += dir[i][0];
                     y += dir[i][1];
-                }else if(arr[x+dir[i][0]][y+dir[i][1]] ==1){
-                    visited[x+dir[i][0]][y+dir[i][1]] =true;
-                    break;
-                }else if(arr[x+dir[i][0]][y+dir[i][1]] ==2){
-                    visited[x+dir[i][0]][y+dir[i][1]] =true;
-                    break;
-                }else if(arr[x+dir[i][0]][y+dir[i][1]] ==3){
-                    visited[x+dir[i][0]][y+dir[i][1]] =true;
-                    x += dir[i][0];
-                    y += dir[i][1];
-                }else{
-                    visited[x+dir[i][0]][y+dir[i][1]] =true;
-                    x += dir[i][0];
-                    y += dir[i][1];
+                }else if(arr[x][y] ==1){
+                    if(i==0 || i==2){
+                        x += dir[i][0];
+                        y += dir[i][1];
+                    }else{
+                        break;
+                    }
+                }else if(arr[x][y] ==2){
+                    if(i==1 || i==3){
+                        x += dir[i][0];
+                        y += dir[i][1];
+                    }else{
+                        break;
+                    }
+                }else if(arr[x][y] ==3){
+                    if(i==0) {
+                        x=x+dir[3][0];
+                        y=y+dir[3][1];
+                        i=3;
+                    }
+                    else if(i==3) {
+                        x=x+dir[0][0]; 
+                        y=y+dir[0][1]; 
+                        i=0;
+                    }
+                    else if(i==1) {
+                        x=x+dir[2][0]; 
+                        y=y+dir[2][1];
+                        i=2;
+                    }
+                    else if(i==2) {
+                        x=x+dir[1][0]; 
+                        y=y+dir[1][1]; 
+                        i=1;
+                    }
+                 }else{
+                    if(i==0) {
+                        x=x+dir[1][0]; 
+                        y=y+dir[1][1]; 
+                        i=1;
+                    }
+                    else if(i==1) {
+                        x=x+dir[0][0]; 
+                        y=y+dir[0][1]; 
+                        i=0;
+                    }
+                    else if(i==2) {
+                        x=x+dir[3][0]; 
+                        y=y+dir[3][1]; 
+                        i=3;
+                    }
+                    else if(i==3) {
+                        x=x+dir[2][0]; 
+                        y=y+dir[2][1]; 
+                        i=2;
+                    }
                 }   
             }
-        }
     }
 }
